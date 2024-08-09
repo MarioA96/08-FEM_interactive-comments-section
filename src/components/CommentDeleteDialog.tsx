15708e9d7b41d7dcf15b7f28ca06f158614070a4
MarioA96 <marioaquirozs@gmail.com>
2024-08-09 13:55:26 -0600

    Se agregaron componentes de comentarios

---
+++ src/components/CommentDeleteDialog.tsx
@@ -0,0 +1,43 @@
+import {
+    DialogClose,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle
+} from "@/components/ui/dialog"
+import { Button } from "./ui/button"
+import { css } from "styled-system/css"
+
+export const CommentDeleteDialog = () => {
+    return (
+        <div id="dialog_container">
+            <DialogContent className="min-w-[150px] max-w-[350px] rounded-lg">
+                <DialogHeader>
+                    <DialogTitle>
+                        <span className={css({ display: 'flex', alignItems: 'flex-start' })}>
+                            Delete comment
+                        </span>
+                    </DialogTitle>
+                    <DialogDescription>
+                        <p className={css({ display: 'flex', textAlign: 'justify', color: 'gray.400', fontWeight: '500' })}>
+                            Are you sure you want to delete this comment? This will remove the comment
+                            and can't be undone.
+                        </p>
+                    </DialogDescription>
+                </DialogHeader>
+                <DialogFooter>
+                    <div className={css({ display: 'flex', justifyContent: 'center', spaceX: '10' })}>
+                        <DialogClose asChild>
+                            <Button className={ css({ backgroundColor: 'gray.500' }) }>
+                                NO, CANCEL
+                            </Button>
+                        </DialogClose>
+                        <Button className={ css({ backgroundColor: 'red.500' }) }>YES, DELETE</Button>
+                    </div>
+                </DialogFooter>
+            </DialogContent>
+        </div>
+        
+    )
+}
---
+++ src/components/CommentGroup.tsx
@@ -0,0 +1,39 @@
+import type { CommentElement, User } from "@/interfaces/comment.interface";
+import { CommentItem } from "./CommentItem";
+
+
+export const CommentGroup = ({comment, currentUser}:{ comment: CommentElement, currentUser: User }) => {
+    
+    let isSelfComment = false;
+
+    if( currentUser.username === comment.user.username ) {isSelfComment = true;}
+    else {isSelfComment = false;}
+    return (
+        <>
+            <CommentItem 
+                user={comment.user} 
+                createdAt={comment.createdAt} 
+                content={comment.content} 
+                score={comment.score}
+                isSelfComment={isSelfComment} 
+            />
+            {
+                comment.replies && comment.replies.map((reply, index) => {
+                    if( currentUser.username === reply.user.username ) {isSelfComment = true;}
+                    else {isSelfComment = false;}
+                    return (
+                        <CommentItem key={index} 
+                            user={reply.user} 
+                            createdAt={reply.createdAt} 
+                            content={reply.content} 
+                            score={reply.score}
+                            replyingTo={reply.replyingTo} 
+                            isSelfComment={isSelfComment}
+                            isReply={true}
+                        />
+                    )
+                })
+            }
+        </>
+    )
+}
