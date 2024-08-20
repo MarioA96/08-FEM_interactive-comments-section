cde683b2ac388bc9688d9f71c205163d73c5cedb
MarioA96 <marioaquirozs@gmail.com>
2024-08-20 14:07:04 -0600

    Se reestructuro el componente para usar ahora un hook personalizado

--- src/components/CommentItem.tsx
+++ src/components/CommentItem.tsx
@@ -1,8 +1,3 @@
-import { useState } from "react";
-
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { useCommentApi } from "@/helpers/useCommentApi";
-
 import { css, cx } from "styled-system/css";
 import { grid, gridItem } from "styled-system/patterns";
 
@@ -26,64 +21,29 @@ import { Button } from "./ui/button";
 import { CommentScore } from "./CommentScore";
 import { CommentActions } from "./CommentActions";
 import type { User } from "@/interfaces/comment.interface";
-
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { z } from "zod";
+import { useCommentItem } from "@/helpers/hooks/useCommentItem";
+import { AutorCommentSection } from "./AutorCommentSection";
 
 
-
-const formSchema = z.object({
-    comment: z.string().min(2, {
-      message: "Comment must be at least 2 characters.",
-    }).max(270,{
-        message: "Comment must be at most 270 characters."
-    }),
-})
 
 export const CommentItem = ({ idCommentParent, idCommentChild, user, createdAt, content, score, replyingTo, isSelfComment, isReply }: { idCommentParent: string, idCommentChild?: string , user: User, createdAt: string, content: string, score: number, replyingTo?: string, isSelfComment: boolean, isReply?: boolean }) => {
-  
-    const [isEditigComment, setIsEditigComment] = useState<boolean>(false);
-    const queryClient = useQueryClient();
-    const commentApi = useCommentApi();
 
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            comment: content,
-        },
-    });
+    const {
+        form, 
+        isEditingComment, 
+        isReplyingParent,
+        isReplyingChild,
 
-    const mutation = useMutation({
-        mutationFn: commentApi.updateComment,
-        onSuccess: () => {
-          // Invalidate and refetch
-          queryClient.invalidateQueries({ queryKey: ['comments'] })
-        },
-    })
+        setIsEditigComment, 
+        setIsReplyingParent,
+        setIsReplyingChild,
 
-    type Parameter = {
-        idCommentParent: string,
-        content: string
-        idCommentChild?: string,
-    };
-    function onSubmit(values: z.infer<typeof formSchema>) {
-        console.log({idCommentParent, idCommentChild, values});
-        
-        const content = values.comment;
-        const parameters: Parameter = idCommentChild 
-                            ? { idCommentParent, content, idCommentChild } 
-                            : { idCommentParent, content };
-        
-        mutation.mutate({...parameters});
-        setIsEditigComment(false);
-    }
-    function handleCancel(event: React.MouseEvent<HTMLButtonElement>) {
-        event.preventDefault(); 
-        setIsEditigComment(false);
-    }
-
-
+        onSubmit, 
+        handleCancel
+    } = idCommentChild 
+                        ? useCommentItem(content, idCommentParent, idCommentChild) 
+                        : useCommentItem(content, idCommentParent);
+    
     return (
         <>
             <div className={ cx( grid({ columns: 12 }), gridItem({ colSpan: 11 }), css({ height: 'fit-content', marginLeft: isReply ? '5px' : '0px' }) ) }>
@@ -118,7 +78,7 @@ export const CommentItem = ({ idCommentParent, idCommentChild, user, createdAt, 
                         <div id="replyComment_content" className="font-sans font-medium text-gray-400 text-[13px]">
                             { replyingTo && ( <span className="text-indigo-600 font-bold">@{ replyingTo }&nbsp;</span> ) } 
                             { 
-                                isEditigComment 
+                                isEditingComment 
                                     ? (
                                         <Form {...form}>
                                             <form onSubmit={form.handleSubmit(onSubmit)}>
@@ -140,8 +100,8 @@ export const CommentItem = ({ idCommentParent, idCommentChild, user, createdAt, 
                                                 />
 
                                                  <div id="replyComment_btn_update" className={ cx( gridItem({ colSpan: 8 }), css({ spaceX: 4, marginTop: 3, marginLeft: 5 }) ) }>
+                                                    <Button onClick={handleCancel} className="bg-red-500">Cancel</Button>
                                                     <Button type="submit" className={ css({ backgroundColor: 'indigo.700' }) }>Update</Button>
-                                                    <Button onClick={handleCancel}>Cancel</Button>
                                                 </div>
 
                                             </form>
@@ -155,13 +115,13 @@ export const CommentItem = ({ idCommentParent, idCommentChild, user, createdAt, 
                     <CardFooter>
                         <div className={ grid({ columns: 12, gap: '6' }) }>
                             {
-                                !isEditigComment && (
+                                !isEditingComment && (
                                     <>
                                         <div id="replyComment_score" className={ cx( gridItem({ colSpan: 4 }), css({ borderRadius: '10px' }) ) }>
                                             <CommentScore score={ score }/>
                                         </div>
                                         <div id="replyComment_btn_actions" className={ gridItem({ colSpan: 8 }) }>
-                                            <CommentActions setIsEditigComment={setIsEditigComment} isSelfComment={isSelfComment} idCommentParent={idCommentParent} idCommentChild={idCommentChild}/>
+                                            <CommentActions setIsReplyingParent={setIsReplyingParent} setIsReplyingChild={setIsReplyingChild} setIsEditigComment={setIsEditigComment} isSelfComment={isSelfComment} idCommentParent={idCommentParent} idCommentChild={idCommentChild}/>
                                         </div>
                                     </>
                                 )
@@ -170,6 +130,27 @@ export const CommentItem = ({ idCommentParent, idCommentChild, user, createdAt, 
                     </CardFooter>
 
                 </Card>
+
+                {
+                    isReplyingParent && (
+                        <>
+                            <div id="lateralSeparator" className={ cx( gridItem({ colSpan: 1 }), css({ height: '100%', width: '2px', backgroundColor: 'gray.400' }) ) }></div>
+                            <div className={ gridItem({ colSpan: 11 }) }>
+                                <AutorCommentSection idCommentParent={idCommentParent} isReplyingParent={isReplyingParent} setIsReplyingParent={setIsReplyingParent} setIsReplyingChild={setIsReplyingChild} replyingToParentUser={user.username}/>
+                            </div>
+                        </>
+                    )
+                }
+                {
+                    isReplyingChild && (
+                        <>
+                            <div id="lateralSeparator" className={ cx( gridItem({ colSpan: 1 }), css({ height: '100%', width: '2px', backgroundColor: 'gray.400' }) ) }></div>
+                            <div className={ gridItem({ colSpan: 11 }) }>
+                                <AutorCommentSection idCommentParent={idCommentParent} idCommentChild={idCommentChild} isReplyingChild={isReplyingChild} setIsReplyingParent={setIsReplyingParent} setIsReplyingChild={setIsReplyingChild} replyingToChildUser={user.username}/>
+                            </div>
+                        </>
+                    )
+                }
             </div>
         </>
     )
