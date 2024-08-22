0afa2e154b361bfd8fdde403404a00747e97bf87
MarioA96 <marioaquirozs@gmail.com>
2024-08-22 14:56:55 -0600

    Se simplifico el uso de estilos mediante un componente y acciones para el score
    Se agregaron funcionalidades básicas para las acciones de score aun por implementar en funcionalidad con el posteo de los mismos en la aplicación

--- src/components/CommentScore.tsx
+++ src/components/CommentScore.tsx
@@ -1,42 +1,124 @@
 
 import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
-import { css } from "styled-system/css";
+import { css, cva } from "styled-system/css";
 
 
-export function CommentScore({ score }: { score: number }) {
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useCommentApi } from "@/helpers/useCommentApi";
 
-  return (
-    <ToggleGroup type="single" className={ 
-        css({ 
-            width: 'fit-content', 
-            height: '30px', 
-            backgroundColor: 'gray.100',
-            borderRadius: '10px'
-        }) 
-    }>
+import { Form } from "@/components/ui/form"
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { useState } from "react";
+
+
+const toggleGroup = cva({
+    base: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        width: 'fit-content',
+        height: '30px',
+        backgroundColor: 'gray.100',
+        borderRadius: '10px',
+        '& span#score_value': {
+            height: '20px', 
+            width: '15px', 
+            marginLeft: '7px', 
+            marginTop: '6px',
+            fontWeight: 'bold', 
+            fontSize: '12px',
+            color: 'hsl(212, 24%, 26%)'
+        },
+        '& #toggleGroupItem': {
+            cursor: 'pointer',
+            '& #span_icon_plus': {
+                fontSize: '16.5px', 
+                fontWeight: 'bolder', 
+                color: 'blue.500'
+            },
+            '& #span_icon_minus': {
+                fontSize: '16.5px', 
+                fontWeight: 'bolder', 
+                color: 'red.500'
+            }
+        }
+    }
+});
+
+
+const formSchema = z.object({
+    value: z.number().int().min(0).max(100),
+})
+
+
+export function CommentScore({ score, idCommentParent, idCommentChild }: { score: number, idCommentParent: string, idCommentChild?: string }) {
+
+    const queryClient = useQueryClient();
+    const commentApi = useCommentApi();
+
+    const [valueScore, setValueScore] = useState<number>(score);
+    const [actualTypeScore, setActualTypeScore] = useState<string>('neutral');
+    //TODO - agregar un valor que indique que deshabilite el boton de votar si ya votó
+    //TODO - agregar un brillo al boton que votó
+    //TODO - agregar un valor que indique que hasBeenRated para que no pueda votar mas de una vez y sepa a quien votó
+    //TODO - agregar en la BD un campo que indique a quien votó y que valor le dió basado en los ID's de los comentarios (array de ids y valores)
+    //TODO - Se podria guardar el rate en un store una vez capturado por tanStack
+    /* 
+        "ratings": {
+            "ratedCommentsIds": [
+                {
+                    "idComment": string,
+                    "typeRate": string
+                }
+            ]
+        }
+    */
+
+    const mutation = useMutation({
+        mutationFn: commentApi.updateComment,
+        onSuccess: () => {
+          // Invalidate and refetch
+          queryClient.invalidateQueries({ queryKey: ['comments'] })
+        },
+    })
+
+    const onScoreChange = (value: number, typeScore: string) => {
+        console.log(value, typeScore);
         
-        <ToggleGroupItem value="plus" aria-label="Toggle plus" className={ css({ cursor: 'pointer' }) }>
-            <span className={ css({ fontSize: '16.5px', fontWeight: 'bolder', color: 'blue.500' }) }>+</span>
-        </ToggleGroupItem>
+        if(typeScore === 'plus') {
+            if(actualTypeScore === 'minus') {
+                setValueScore(value + 2);
+                setActualTypeScore('plus');
+            } else {
+                setValueScore(value + 1);
+                setActualTypeScore('plus');
+            }
+        } else {
+            if(actualTypeScore === 'plus') {
+                setValueScore(value - 2);
+                setActualTypeScore('minus');
+            } else {
+                setValueScore(value - 1);
+                setActualTypeScore('minus');
+            }
+        }
+    }
 
-        <span className={ 
-            css({ 
-                height: '20px', 
-                width: '15px', 
-                marginLeft: '7px', 
-                marginTop: '6px',
-                fontWeight: 'bold', 
-                fontSize: '12px',
-                color: 'hsl(212, 24%, 26%)' 
-            }) 
-        }>
-            { score }
-        </span>
+    return (
+        <ToggleGroup type="single" className={ toggleGroup({}) }>
+            <ToggleGroupItem id="toggleGroupItem" value="plus" aria-label="Toggle plus" onClick={() => onScoreChange(valueScore, 'plus')}>
+                <span id="span_icon_plus">+</span>
+            </ToggleGroupItem>
 
-        <ToggleGroupItem value="minus" aria-label="Toggle minus" className={ css({ cursor: 'pointer' }) }>
-            <span className={ css({ fontSize: '19px', fontWeight: 'bolder', color: 'red.500' }) }>-</span>
-        </ToggleGroupItem>
+            <span id="score_value">
+                { valueScore }
+            </span>
 
-    </ToggleGroup>
-  );
+            <ToggleGroupItem id="toggleGroupItem" value="minus" aria-label="Toggle minus" onClick={() => onScoreChange(valueScore, 'minus')}>
+                <span id="span_icon_minus">-</span>
+            </ToggleGroupItem>
+        </ToggleGroup>
+    );
 }
