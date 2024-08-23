90199666021ce7e8caa15b4e9fbb7b20d92ff1e9
MarioA96 <marioaquirozs@gmail.com>
2024-08-23 14:56:31 -0600

    Se agrego la funcionalidad para la modificación de score
    Se estableció la posibilidad de realizar un rating a un comentario y este modificar el estado de la BD con visualización inmediata en la app

--- src/components/CommentScore.tsx
+++ src/components/CommentScore.tsx
@@ -6,11 +6,8 @@ import { css, cva } from "styled-system/css";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useCommentApi } from "@/helpers/useCommentApi";
 
-import { Form } from "@/components/ui/form"
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { z } from "zod";
 import { useState } from "react";
+import { useScoreApi } from "@/helpers/useScoreApi";
 
 
 const toggleGroup = cva({
@@ -48,67 +45,92 @@ const toggleGroup = cva({
 });
 
 
-const formSchema = z.object({
-    value: z.number().int().min(0).max(100),
-})
-
-
 export function CommentScore({ score, idCommentParent, idCommentChild }: { score: number, idCommentParent: string, idCommentChild?: string }) {
 
     const queryClient = useQueryClient();
     const commentApi = useCommentApi();
+    const scoreApi = useScoreApi();
 
     const [valueScore, setValueScore] = useState<number>(score);
-    const [actualTypeScore, setActualTypeScore] = useState<string>('neutral');
-    //TODO - agregar un valor que indique que deshabilite el boton de votar si ya votó
-    //TODO - agregar un brillo al boton que votó
-    //TODO - agregar un valor que indique que hasBeenRated para que no pueda votar mas de una vez y sepa a quien votó
-    //TODO - agregar en la BD un campo que indique a quien votó y que valor le dió basado en los ID's de los comentarios (array de ids y valores)
+    const [actualTypeRate, setActualTypeRate] = useState<string>('neutral');
+    const [hasBeenRated, setHasBeenRated] = useState<boolean[]>([false, false]); //[plus, minus]
+    /////TODO - agregar un valor que indique que deshabilite el boton de votar si ya votó
+    /////TODO - agregar un brillo al boton que votó
+    /////TODO - agregar un valor que indique que hasBeenRated para que no pueda votar mas de una vez y sepa a quien votó
+    /////TODO - agregar en la BD un campo que indique a quien votó y que valor le dió basado en los ID's de los comentarios (array de ids y valores)
+    /////TODO - se debe de actualizar el score del comentario en la BD -> /comments
+    //TODO - se debe de actualizar a que comentario se le está votando (rate) en la BD -> /score (si typeScore es neutral, se quita)
     //TODO - Se podria guardar el rate en un store una vez capturado por tanStack
-    /* 
-        "ratings": {
-            "ratedCommentsIds": [
-                {
-                    "idComment": string,
-                    "typeRate": string
-                }
-            ]
-        }
-    */
 
-    const mutation = useMutation({
-        mutationFn: commentApi.updateComment,
+    const mutationScore = useMutation({
+        mutationFn: () => (
+            idCommentChild 
+                ? commentApi.updateCommentScore(valueScore, idCommentParent, idCommentChild) 
+                : commentApi.updateCommentScore(valueScore, idCommentParent)
+        ),
         onSuccess: () => {
           // Invalidate and refetch
           queryClient.invalidateQueries({ queryKey: ['comments'] })
         },
     })
 
+    const mutationRating = useMutation({
+        mutationFn: () => (
+            idCommentChild 
+                ? scoreApi.postRate(actualTypeRate, idCommentParent, idCommentChild) 
+                : scoreApi.postRate(actualTypeRate, idCommentParent)
+        ),
+        onSuccess: () => {
+          // Invalidate and refetch
+          queryClient.invalidateQueries({ queryKey: ['ratings'] })
+        },
+    })
+
     const onScoreChange = (value: number, typeScore: string) => {
-        console.log(value, typeScore);
-        
         if(typeScore === 'plus') {
-            if(actualTypeScore === 'minus') {
+            if(actualTypeRate === 'minus') {
                 setValueScore(value + 2);
-                setActualTypeScore('plus');
-            } else {
+                setActualTypeRate('plus');
+                setHasBeenRated([true, false]);
+                mutationScore.mutate();
+            }
+            else if(actualTypeRate === 'plus'){ //Prev plus, to cancel score
+                setValueScore(value - 1);
+                setActualTypeRate('neutral');
+                setHasBeenRated([false, false]);
+                mutationScore.mutate();
+            }
+            else { //Neutral
                 setValueScore(value + 1);
-                setActualTypeScore('plus');
+                setActualTypeRate('plus');
+                setHasBeenRated([true, false]);
+                mutationScore.mutate();
             }
         } else {
-            if(actualTypeScore === 'plus') {
+            if(actualTypeRate === 'plus') {
                 setValueScore(value - 2);
-                setActualTypeScore('minus');
-            } else {
+                setActualTypeRate('minus');
+                setHasBeenRated([false, true]);
+                mutationScore.mutate();
+            }
+            else if(actualTypeRate === 'minus'){ //Prev minus, to cancel score
+                setValueScore(value + 1);
+                setActualTypeRate('neutral');
+                setHasBeenRated([false, false]);
+                mutationScore.mutate();
+            } 
+            else { //Neutral
                 setValueScore(value - 1);
-                setActualTypeScore('minus');
+                setActualTypeRate('minus');
+                setHasBeenRated([false, true]);
+                mutationScore.mutate();
             }
         }
     }
 
     return (
         <ToggleGroup type="single" className={ toggleGroup({}) }>
-            <ToggleGroupItem id="toggleGroupItem" value="plus" aria-label="Toggle plus" onClick={() => onScoreChange(valueScore, 'plus')}>
+            <ToggleGroupItem id="toggleGroupItem" value="plus" className={css({ boxShadow: hasBeenRated[0] ? '0px 0px 9px 2px rgba(4,31,224,0.79)' : ''  })} aria-label="Toggle plus" onClick={() => onScoreChange(valueScore, 'plus')}>
                 <span id="span_icon_plus">+</span>
             </ToggleGroupItem>
 
@@ -116,7 +138,7 @@ export function CommentScore({ score, idCommentParent, idCommentChild }: { score
                 { valueScore }
             </span>
 
-            <ToggleGroupItem id="toggleGroupItem" value="minus" aria-label="Toggle minus" onClick={() => onScoreChange(valueScore, 'minus')}>
+            <ToggleGroupItem id="toggleGroupItem" value="minus" className={css({ boxShadow: hasBeenRated[1] ? '0px 0px 9px 2px rgba(224,4,61,0.79)' : ''  })} aria-label="Toggle minus" onClick={() => onScoreChange(valueScore, 'minus')}>
                 <span id="span_icon_minus">-</span>
             </ToggleGroupItem>
         </ToggleGroup>
