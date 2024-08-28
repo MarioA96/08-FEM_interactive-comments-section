836a5fc752765fffa3d989053ad6a15ff3aebd8a
MarioA96 <marioaquirozs@gmail.com>
2024-08-28 16:33:28 -0600

    Se agregaron las configuraciones necesarias para el deploy del proyecto

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://marioa96.github.io/',
+  base: '08-FEM_interactive-comments-section',
   integrations: [react()]
 });
